2a6b31f2b06c4bef108bf2ba568b1635c10575b0
TRIXServer <admin@trixserver.com>
2022-06-30 09:02:07 -0300

    commit

--- datos.cs
+++ datos.cs
@@ -18,7 +18,7 @@ public class datos
     public int Edad { get => edad; set => edad = value; }
     public int Salud { get => salud; set => salud = value; }
 
-    public datos(rootNames datanames)
+    public datos(rootNames dataNames)
     {
         var random = new Random();
 
@@ -35,6 +35,48 @@ public class datos
             "Spy"
         };
 
-        
+        var aliasPersonaje = new string[]
+        {
+            "Red Reaper",
+            "Gunner",
+            "Doomsday",
+            "Dark Sword",
+            "Insidious",
+            "God's Punisher",
+            "War Dog",
+            "Iron Hand",
+            "Blood Star",
+            "Atomic Arsenic",
+            "Atomic Apple",
+            "Purple Sun",
+            "Anger",
+            "Guts Destructor",
+            "Ego Monster"
+        };
+
+        int nombreRandom = random.Next(0, dataNames.Results.Count());
+        tipo = tipoPersonaje[random.Next(0, tipoPersonaje.Length)];
+        nombre = $"{dataNames.Results[nombreRandom]} {dataNames.Results[nombreRandom].Name.Last}";
+        alias = aliasPersonaje[random.Next(0, aliasPersonaje.Length)];
+        var fecha = new DateOnly(random.Next(1723, 2023), random.Next(1, 13), random.Next(1, 29));
+        fechaNacimiento = Convert.ToString(fecha);
+        edad = DateTime.Now.Year - fecha.Year;
+        if (fecha.Month > DateTime.Now.Month)
+        {
+            edad--;
+
+        }
+        else
+        {
+            if (fecha.Month == DateTime.Now.Month && fecha.Day > DateTime.Now.Day)
+            {
+                edad--;
+
+            }
+
+        }
+        salud = 100;
+
     }
+
 }
